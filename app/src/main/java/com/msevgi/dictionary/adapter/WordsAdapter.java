45e1a434a99c0ee112110aefc77e4e757a985859
mustafasevgi <m_sevgi@hotmail.com>
2014-10-13 23:17:52 +0300

    search kısmen eklendi

--- app/src/main/java/com/msevgi/dictionary/adapter/WordsAdapter.java
+++ app/src/main/java/com/msevgi/dictionary/adapter/WordsAdapter.java
@@ -1,11 +1,13 @@
-
 package com.msevgi.dictionary.adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import com.msevgi.dictionary.R;
@@ -13,51 +15,93 @@ import com.msevgi.dictionary.model.Words;
 import com.msevgi.dictionary.provider.BusProvider;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class WordsAdapter extends ArrayAdapter<Words> {
-   private ViewHolder mViewHolder;
+public class WordsAdapter extends ArrayAdapter<Words> implements Filterable {
+    private ViewHolder mViewHolder;
+    private static List<Words> mOriginItmeList;
+    private static List<Words> mItemList;
 
-   public WordsAdapter(Context context, int resource, ArrayList<Words> objects) {
-      super(context, resource, objects);
+    public WordsAdapter(Context context, int resource, ArrayList<Words> objects) {
+        super(context, resource, objects);
+        mOriginItmeList = mItemList = objects;
 
-   }
+    }
 
-   @Override
-   public View getView(int position, View convertView, ViewGroup parent) {
-      if (convertView == null) {
-         convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.word_item, parent, false);
-         mViewHolder = new ViewHolder(convertView);
-         convertView.setTag(mViewHolder);
-      }
-      else
-         mViewHolder = (ViewHolder) convertView.getTag();
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.word_item, parent, false);
+            mViewHolder = new ViewHolder(convertView);
+            convertView.setTag(mViewHolder);
+        } else
+            mViewHolder = (ViewHolder) convertView.getTag();
 
-      final Words mItem = getItem(position);
-      if (mItem != null) {
-         mViewHolder.mTextViewEnglish.setText(mItem.getEnglish_word());
-         mViewHolder.mTextViewTurkish.setText(mItem.getTurkish_word());
-         convertView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-               BusProvider.getInstance().post(mItem);
-               return false;
+        final Words mItem = getItem(position);
+        if (mItem != null) {
+            mViewHolder.mTextViewEnglish.setText(mItem.getEnglish_word());
+            mViewHolder.mTextViewTurkish.setText(mItem.getTurkish_word());
+            convertView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    BusProvider.getInstance().post(mItem);
+                    return false;
+                }
+            });
+        }
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return mItemList.size();
+    }
+
+    protected final class ViewHolder {
+        @InjectView(R.id.textview_turkish_word)
+        TextView mTextViewTurkish;
+        @InjectView(R.id.textview_english_word)
+        TextView mTextViewEnglish;
+
+        public ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
+    }
+
+    @Override
+    public Filter getFilter() {
+        return super.getFilter();
+    }
+
+    private class MyFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+            String search = constraint.toString();
+            if (!TextUtils.isEmpty(search)) {
+                results.values = mOriginItmeList;
+                results.count = mOriginItmeList.size();
+            } else {
+                ArrayList<Words> tempList = new ArrayList<Words>();
+                for (Words item : mOriginItmeList) {
+                    if (item.getEnglish_word().contains(search) || item.getTurkish_word().contains(search)) {
+                        tempList.add(item);
+                    }
+                }
+                results.values = tempList;
+                results.count = tempList.size();
             }
-         });
-      }
-      return convertView;
-   }
+            return results;
+        }
 
-   protected final class ViewHolder {
-      @InjectView(R.id.textview_turkish_word)
-      TextView mTextViewTurkish;
-      @InjectView(R.id.textview_english_word)
-      TextView mTextViewEnglish;
-
-      public ViewHolder(View view) {
-         ButterKnife.inject(this, view);
-      }
-   }
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            mItemList = (ArrayList<Words>) results.values;
+            notifyDataSetChanged();
+        }
+    }
 }
--- app/src/main/java/com/msevgi/dictionary/fragment/FragmentGetWords.java
+++ app/src/main/java/com/msevgi/dictionary/fragment/FragmentGetWords.java
@@ -1,8 +1,9 @@
-
 package com.msevgi.dictionary.fragment;
 
 import android.app.AlertDialog;
+import android.app.SearchManager;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -11,8 +12,11 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.msevgi.dictionary.R;
 import com.msevgi.dictionary.adapter.WordsAdapter;
@@ -27,98 +31,127 @@ import java.util.ArrayList;
 import butterknife.InjectView;
 
 public class FragmentGetWords extends BaseFragment implements LoaderManager.LoaderCallbacks<Cursor> {
-   @InjectView(R.id.listview_words)
-   public ListView      mListView;
-   private WordsAdapter mAdapter;
+    @InjectView(R.id.listview_words)
+    public ListView mListView;
+    private WordsAdapter mAdapter;
 
-   @Override
-   public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      // LoaderManager manages loaders associated with an Activity or a Fragment.
-      // CursorLoader ensures queries are performed asynchronously.
-      getLoaderManager().initLoader(0, null, this); // Third parameter is reference to callbacks
-      setHasOptionsMenu(true);
-      // BusProvider.getInstance().register(this);
-      mAdapter = new WordsAdapter(getActivity(), R.layout.word_item, new ArrayList<Words>());
-      mAdapter.setNotifyOnChange(true);
-   }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // LoaderManager manages loaders associated with an Activity or a Fragment.
+        // CursorLoader ensures queries are performed asynchronously.
+        getLoaderManager().initLoader(0, null, this); // Third parameter is reference to callbacks
+        setHasOptionsMenu(true);
+        // BusProvider.getInstance().register(this);
+        mAdapter = new WordsAdapter(getActivity(), R.layout.word_item, new ArrayList<Words>());
+        mAdapter.setNotifyOnChange(true);
+    }
 
-   @Override
-   public void onViewCreated(View view, @Nullable
-   Bundle savedInstanceState) {
-      super.onViewCreated(view, savedInstanceState);
-      mListView.setAdapter(mAdapter);
-   }
+    @Override
+    public void onViewCreated(View view, @Nullable
+    Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mListView.setAdapter(mAdapter);
+    }
 
-   @Override
-   public void onResume() {
-      super.onResume();
-      BusProvider.getInstance().register(this);
-      getLoaderManager().restartLoader(0, null, this); // Third parameter is reference to callbacks
-   }
+    @Override
+    public void onResume() {
+        super.onResume();
+        BusProvider.getInstance().register(this);
+        getLoaderManager().restartLoader(0, null, this); // Third parameter is reference to callbacks
+    }
 
-   @Override
-   public void onPause() {
-      super.onPause();
-      BusProvider.getInstance().unregister(this);
-   }
+    @Override
+    public void onPause() {
+        super.onPause();
+        BusProvider.getInstance().unregister(this);
+    }
 
-   @NonNull
-   @Override
-   protected int getLayoutResource() {
-      return R.layout.fragment_get_words;
-   }
+    @NonNull
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.fragment_get_words;
+    }
 
-   @Subscribe
-   public void onListViewItemLongClick(final Words item) {
-      new AlertDialog.Builder(getActivity()).setTitle(R.string.warning)
-                                            .setMessage(R.string.delete_word)
-                                            .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                                               @Override
-                                               public void onClick(DialogInterface dialog, int which) {
-                                                  dialog.dismiss();
-                                               }
-                                            })
-                                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                                               @Override
-                                               public void onClick(DialogInterface dialog, int which) {
-                                                  mAdapter.remove(item);
-                                                  ContentResolver cr = getActivity().getContentResolver();
-                                                  cr.delete(MyContentProvider.CONTENT_URI,
-                                                            "_id = ?",
-                                                            new String[]{String.valueOf(item.getId())});
-                                                  dialog.dismiss();
-                                               }
-                                            })
-                                            .show();
-   }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.search, menu);
+        SearchManager mManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
 
-   @Override
-   public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-      return new CursorLoader(getActivity(), MyContentProvider.CONTENT_URI, null, null, null, null);
-   }
+        SearchView mSearch = (SearchView) menu.findItem(R.id.search).getActionView();
 
-   @Override
-   public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-      mAdapter.clear();
-      // Gets index of the column given a name.
-      final int mTurkishColoumn = cursor.getColumnIndexOrThrow(MySQLiteOpenHelper.TURKISH_WORD_COLUMN);
-      final int mEnglishColoumn = cursor.getColumnIndexOrThrow(MySQLiteOpenHelper.ENGLISH_WORD_COLUMN);
-      final int mID = cursor.getColumnIndexOrThrow(MySQLiteOpenHelper.ID_COLUMN);
+        mSearch.setSearchableInfo(mManager.getSearchableInfo(getActivity().getComponentName()));
 
-      // Database queries are returned as Cursor objects.
-      // Cursors are pointers to the result set within the underlying data.
-      // Here is how to iterate over the cursor rows.
-      while (cursor.moveToNext()) { // Moves cursor to next row, cursor is initialized at before first.
-         final String mTurkishWord = cursor.getString(mTurkishColoumn); // Extract column data from cursor.
-         final String mEnglishWord = cursor.getString(mEnglishColoumn); // Extract column data from cursor.
-         final int id = cursor.getInt(mID);
-         mAdapter.add(new Words(id, mTurkishWord, mEnglishWord));
-      }
-   }
+        mSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
-   @Override
-   public void onLoaderReset(Loader<Cursor> cursorLoader) {
-      mAdapter.clear();
-   }
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+
+                mAdapter.getFilter().filter(query);
+
+                return true;
+
+            }
+
+        });
+    }
+
+    @Subscribe
+    public void onListViewItemLongClick(final Words item) {
+        new AlertDialog.Builder(getActivity()).setTitle(R.string.warning)
+                .setMessage(R.string.delete_word)
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mAdapter.remove(item);
+                        ContentResolver cr = getActivity().getContentResolver();
+                        cr.delete(MyContentProvider.CONTENT_URI,
+                                "_id = ?",
+                                new String[]{String.valueOf(item.getId())});
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(getActivity(), MyContentProvider.CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        mAdapter.clear();
+        // Gets index of the column given a name.
+        final int mTurkishColoumn = cursor.getColumnIndexOrThrow(MySQLiteOpenHelper.TURKISH_WORD_COLUMN);
+        final int mEnglishColoumn = cursor.getColumnIndexOrThrow(MySQLiteOpenHelper.ENGLISH_WORD_COLUMN);
+        final int mID = cursor.getColumnIndexOrThrow(MySQLiteOpenHelper.ID_COLUMN);
+
+        // Database queries are returned as Cursor objects.
+        // Cursors are pointers to the result set within the underlying data.
+        // Here is how to iterate over the cursor rows.
+        while (cursor.moveToNext()) { // Moves cursor to next row, cursor is initialized at before first.
+            final String mTurkishWord = cursor.getString(mTurkishColoumn); // Extract column data from cursor.
+            final String mEnglishWord = cursor.getString(mEnglishColoumn); // Extract column data from cursor.
+            final int id = cursor.getInt(mID);
+            mAdapter.add(new Words(id, mTurkishWord, mEnglishWord));
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        mAdapter.clear();
+    }
 }
